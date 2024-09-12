9e4815c3fbcb46dee5563fbe8893198b6bd2d75e
Simon Cooper <simon.cooper@elastic.co>
2024-09-12 11:11:07 +0100

    Change default locale of date mappers to ENGLISH

--- server/src/main/java/org/elasticsearch/index/mapper/DateFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DateFieldMapper.java
@@ -235,7 +235,7 @@ public final class DateFieldMapper extends FieldMapper {
         private final Parameter<Locale> locale = new Parameter<>(
             "locale",
             false,
-            () -> Locale.ROOT,
+            () -> Locale.ENGLISH,
             (n, c, o) -> LocaleUtils.parse(o.toString()),
             m -> toType(m).locale,
             (xContentBuilder, n, v) -> xContentBuilder.field(n, v.toString()),
